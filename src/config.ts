3736999fb143d4c895927211173070bc5b383b89
harleyszhang <zhg5200211@outlook.com>
2024-10-28 17:48:01 +0800

    feat: update blogPage

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const SITE = {
   icon: 'https://gw.alicdn.com/imgextra/i2/O1CN01m9YYjS1QBeW5DOm3I_!!6000000001938-2-tps-400-400.png',
   pic: 'https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png',
   homePage: 'https://zhg-weekly.vercel.app/',
-  blogPage: 'https://www.cnblogs.com/armcvai',
+  blogPage: 'https://harleyszhang-github-io.vercel.app/',
   zhihuId: 'tang-fen-44-49',
   githubId: 'HarleysZhang',
   repo: 'harleyszhang/zhg_weekly',
